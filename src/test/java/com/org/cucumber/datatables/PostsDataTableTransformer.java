a44b574116091c011a39f8fb8fbd98865781b824
Ubuntu 18.04 LTS <hongiaher@gmail.com>
2020-04-12 17:18:18 -0400

    Datatable feature done.

---
+++ src/test/java/com/org/cucumber/datatables/PostsDataTableTransformer.java
@@ -0,0 +1,27 @@
+package com.org.cucumber.datatables;
+
+import com.org.api.rest.model.Posts;
+import io.cucumber.java.DataTableType;
+
+import java.util.Map;
+
+public class PostsDataTableTransformer {
+
+    private final String USER_ID = "userId";
+
+    private final String ID = "id";
+
+    private final String TITLE = "title";
+
+    private final String BODY = "body";
+
+    @DataTableType
+    public Posts definePosts(Map<String, String> entry) {
+        int userId = Integer.parseInt(entry.get(USER_ID));
+        int id = Integer.parseInt(entry.get(ID));
+        String title = entry.get(TITLE);
+        String body = entry.get(BODY);
+        return new Posts(userId, id, title, body);
+    }
+
+}
