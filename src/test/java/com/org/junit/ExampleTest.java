bc3c05c88394bc1c57615264948940cf07689a11
Ubuntu 18.04 LTS <hongiaher@gmail.com>
2021-01-08 14:17:35 -0500

    Removing pom packaging and adding example junit test.

---
+++ src/test/java/com/org/junit/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.org.junit;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        System.out.println("This is a test of your local broadcasting system.");
+    }
+
+}
